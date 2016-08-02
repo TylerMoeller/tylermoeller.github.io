23905768bbd210ccad3b0c01c4f1fba677a0966a
TylerMoeller <tylermoeller1@gmail.com>
2016-08-01 18:11:07 -0700

    updating project descriptions

--- js/tyler.js
+++ js/tyler.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   $('body').scrollspy({ target: '.navbar-fixed-top', offset: 51 });
 
+  // offset top padding when clicking a navbar link
   $('#topNav').affix({ offset: { top: 100 } });
 
+  // smooth scrolling
   $('.page-scroll a').bind('click', function (e) {
       var $that = $(this);
       $('html, body').stop().animate({
@@ -10,4 +12,10 @@ $(document).ready(function () {
         }, 1250, 'easeInOutExpo');
       e.preventDefault();
     });
+
+  // close modals on 'esc'
+  $('body').keyup(function (e) {
+    var key = e.keyCode || e.which;
+    if (key === 27) $('.modal').modal('hide');
+  });
 });
