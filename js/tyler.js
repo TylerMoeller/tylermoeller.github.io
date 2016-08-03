efeeeab0fa42fd9e2591eb86a79c4ec073010fe7
TylerMoeller <tylermoeller1@gmail.com>
2016-08-03 15:01:27 -0700

    Dynamically generate repository grid
    Simplified addition of new repositories with a new .json file and
    dynamically generated repository grid.

--- js/tyler.js
+++ js/tyler.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
-  $('body').scrollspy({ target: '.navbar-fixed-top', offset: 51 });
-
   // offset top padding when clicking a navbar link
   $('#topNav').affix({ offset: { top: 100 } });
 
   // smooth scrolling
-  $('.page-scroll a').bind('click', function (e) {
-      var $that = $(this);
+  $('.page-scroll a').click(function (e) {
       $('html, body').stop().animate({
-          scrollTop: ($($that.attr('href')).offset().top - 50)
+          scrollTop: ($($(this).attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
       e.preventDefault();
     });
@@ -18,4 +15,67 @@ $(document).ready(function () {
     var key = e.keyCode || e.which;
     if (key === 27) $('.modal').modal('hide');
   });
+
+  buildRepoGrid();
 });
+
+// loop through repos.json to build the image grid
+function buildRepoGrid() {
+  $.getJSON('repos.json').done(function (data) {
+    data.repoList.forEach(function (repo) {
+      var langHTML = '';
+      repo.langs.forEach(function (lang) {
+        langHTML += '<span>' + lang + '</span>';
+      });
+
+      $('#repo-row').append(
+        '<div class="col-sm-6 col-md-4 repo-item">' +
+          '<a href="#' + repo.id + '" class="repo-link" data-toggle="modal">' +
+            '<div class="caption">' +
+              '<div class="caption-content">' +
+                '<i class="fa fa-search-plus fa-3x"></i>' +
+              '</div>' +
+            '</div>' +
+            '<img src="' + repo.screenshot + '" class="img-responsive"' +
+                ' alt="' + repo.altText + '">' +
+            '<div class="carousel-caption">' +
+              repo.name +
+            '</div>' +
+          '</a>' +
+        '</div>' +
+        '<div class="repo-modal modal fade" id="' + repo.id + '"' +
+            ' tabindex="-1" role="dialog" aria-hidden="true">' +
+          '<div class="modal-content">' +
+            '<div class="container">' +
+              '<div class="row">' +
+                '<div class="col-lg-8 col-lg-offset-2">' +
+                  '<div class="modal-body">' +
+                    '<h2 class="repo-modal-title">' + repo.name + '</h2>' +
+                    '<hr class="gh-logo-primary">' +
+                    '<a href="' + repo.url + '">' +
+                      '<img src="' + repo.screenshot + '"' +
+                          ' class="img-thumbnail img-responsive center-block"' +
+                          ' alt="' + repo.altText + '">' +
+                    '</a>' +
+                    '<p>' + repo.description + '</p>' +
+                    '<div class="row">' +
+                      '<div class="col-xs-8 col-xs-offset-2">' +
+                        '<p><strong>Created</strong>: ' + repo.created + '</p>' +
+                      '</div>' +
+                    '</div>' +
+                    '<ul class="list-inline pager">' +
+                      '<li>' + langHTML + '</li>' +
+                    '</ul>' +
+                    '<button type="button" class="btn btn-close" data-dismiss="modal">' +
+                      '<i class="fa fa-times"></i> Close' +
+                    '</button>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      );
+    });
+  });
+}
